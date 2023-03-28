61b12a07750dd641eac2fd5417b873706b9f8f16
Tarun <70534772+tarundevelops@users.noreply.github.com>
2023-03-28 11:41:22 +0530

    Update logout.js

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -3,7 +3,7 @@ import cookie from "cookie";
 export default function handler(req,res){
 
 
-    res.setHeader("Set-Cookie",cookie.serialize("authtoken","",{maxAge:2,secure:true,httpOnly:true,path:"/"}))
+    res.setHeader("Set-Cookie",cookie.serialize("authtoken","",{secure:true,httpOnly:true,path:"/"}))
 
     res.status(200).json({sucess:"sucess"})
 }
